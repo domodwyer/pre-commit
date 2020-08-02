cdc9a80af6d8a9ba1b380f129533de40344529f5
Dom <dom@itsallbroken.com>
2020-08-02 22:34:09 +0100

    r-lintr: add lintr support

--- run-r-lintr.R
+++ run-r-lintr.R
@@ -1,11 +1,19 @@
 #!/usr/bin/env Rscript
 
 files <- commandArgs(trailingOnly=TRUE)
+stop <- FALSE
 tryCatch(
   {
     for (f in files) {
-      lintr::lint(f)
+      errors <- lintr::lint(f)
+      if (length(errors) > 0) {
+        print(errors)
+        stop <- TRUE
+      } 
     }
   }, 
   warning = function(w) stop(conditionMessage(w), call. = FALSE)
 )
+if (stop) {
+  stop("lintr failures found")
+}
