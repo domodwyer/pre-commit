e43b3dfbd8bd86601ba757c9cb6ce0567489c778
Dom <dom@itsallbroken.com>
2021-01-04 16:28:34 +0000

    rust-clippy: remove default lints
    Removes the default lints from the clippy exec.
    
    Users should instead prefer to define them in source, or explicitly as args.
    Previously defining them in the source was then overridden by the args, which
    was confusing and produced the wrong results.

--- run-rust-clippy.sh
+++ run-rust-clippy.sh
@@ -2,21 +2,17 @@
 
 set -euo pipefail
 
-default_lints=( 
-	"-D rust_2018_idioms" 
-	"-D missing_debug_implementations" 
-	"-D unreachable_pub" 
-	"-D missing_docs"
-	"-A clippy::missing_docs_in_private_items" 
-	"-D clippy::doc_markdown" 
-	"-D clippy::todo" 
-	"-D clippy::dbg_macro" 
-	"-D clippy::unimplemented" 
-	"-D clippy::await_holding_lock"
-	"-D clippy::match-like-matches-macro" 
-)
+# Some sensible defaults:
+# "-D rust_2018_idioms" 
+# "-D missing_debug_implementations" 
+# "-D unreachable_pub" 
+# "-D missing_docs"
+# "-A clippy::missing_docs_in_private_items" 
+# "-D clippy::doc_markdown" 
+# "-D clippy::todo" 
+# "-D clippy::dbg_macro" 
+# "-D clippy::unimplemented" 
+# "-D clippy::await_holding_lock"
+# "-D clippy::match-like-matches-macro" 
 
-# Use the lints passed as arguments, or the default above.
-lints=${*:-${default_lints[*]}}
-
-cargo clippy --all-targets --all-features -- -D warnings ${lints}
+cargo clippy --all-targets --all-features -- -D warnings "$@"
